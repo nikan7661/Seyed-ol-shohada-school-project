f34934386cd58924a55e9fd831939452b2478ee2
nikan7661 <156349260+nikan7661@users.noreply.github.com>
2025-09-10 11:40:50 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll("nav ul li a");
+
+    links.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute("href"));
+            window.scrollTo({
+                top: target.offsetTop - 50,
+                behavior: "smooth"
+            });
+        });
+    });
+
+    console.log("Tehran Blog loaded successfully.");
+});
